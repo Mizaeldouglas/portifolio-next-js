f4598725fd9818ac32fda65191f6013ea5c4eb12
Mizaeldouglas <mizaeldouglas2@gmail.com>
2022-02-21 22:56:02 -0300

    Adicionado portifolio

--- pages/Portifolio/index.jsx
+++ pages/Portifolio/index.jsx
@@ -1,94 +1,80 @@
-import { Container } from './styles';
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
-function srcset(image, width, height, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${width * cols}&h=${
-      height * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
+export default function Portifolio() {
+  return (
+		<Container maxWidth="sm" sx={{ marginLeft: 10}} >
+			<ImageList sx={{ width: 800, height: 550,overflowY: 'initial' }}>
+			{itemData.map((item) => (
+				<ImageListItem sx={{margin:1}} key={item.img}>
+				<img
+					src={`${item.img}?w=248&fit=crop&auto=format`}
+					srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+					alt={item.title}
+					loading="lazy"
+				/>
+				<a href={item.link} >
+					<ImageListItemBar
+						title={item.title}
+						// subtitle={<span>by: {item.author}</span>}
+						position="bottom"
+					/>
+				</a>
+				</ImageListItem>
+			))}
+			</ImageList>
+		</Container>	
+  );
 }
 
-function Portifolio() {
-	
-  return (
-    <Container>
-		
-    </Container>
-  );
-};
-
 const itemData = [
-	{
-	  img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-	  title: 'Breakfast',
-	  author: '@bkristastucchio',
-	  featured: true,
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-	  title: 'Burger',
-	  author: '@rollelflex_graphy726',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-	  title: 'Camera',
-	  author: '@helloimnik',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-	  title: 'Coffee',
-	  author: '@nolanissac',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-	  title: 'Hats',
-	  author: '@hjrc33',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-	  title: 'Honey',
-	  author: '@arwinneil',
-	  featured: true,
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-	  title: 'Basketball',
-	  author: '@tjdragotta',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-	  title: 'Fern',
-	  author: '@katie_wasserman',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-	  title: 'Mushrooms',
-	  author: '@silverdalex',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-	  title: 'Tomato basil',
-	  author: '@shelleypauls',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-	  title: 'Sea star',
-	  author: '@peterlaster',
-	},
-	{
-	  img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-	  title: 'Bike',
-	  author: '@southside_customs',
-	},
-  ];
-
-
-
-export default Portifolio;
+  {
+    img: '/01.gif',
+    title: 'ToDo List',
+	link: 'https://github.com/Mizaeldouglas/To-do-List-Em-ReactJS',
+  },
+  {
+    img: '/02.gif',
+    title: 'Galeria de Fotos',
+	link: 'https://github.com/Mizaeldouglas/GalleryPhotos_reactJS',
+  },
+  {
+    img: '/03.png',
+    title: 'Chat-Online',
+	link: 'https://github.com/Mizaeldouglas/Chat-react-Js',
+  },
+  {
+    img: '/04.png',
+    title: 'Site-Contabilidade',
+	link: 'https://github.com/Mizaeldouglas/DevContabil--treino',
+  },
+  {
+    img: '/05.png',
+    title: 'Page-Tribute',
+	link: 'https://github.com/Mizaeldouglas/Tribute-Page_JJR-Tolkien',
+  },
+  {
+    img: '/06.png',
+    title: 'Urna-Eletronica',
+	link: 'https://github.com/Mizaeldouglas/UrnaEletronica',
+  },
+  {
+    img: '/07.png',
+    title: 'Sistema de Login',
+	link: 'https://github.com/Mizaeldouglas/FormularioB7Web',
+  },
+  {
+    img: '/09.png',
+    title: 'Site-Pizzaria',
+	link: 'https://github.com/Mizaeldouglas/EXERCICIO-Compra-de-Pizzas',
+  },
+  {
+    img: '/08.png',
+    title: 'App Lista de series Mobile',
+	link: 'https://github.com/Mizaeldouglas/ListaFilmes-React-Native',
+  },
+];
